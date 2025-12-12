d815b3880b7536a0b63d9b3738dc127a0dbbc38d
Karthik Vishal <karthikvishal1506@gmail.com>
2025-12-12 10:37:44 +0530

    Added attacker file for simul..

---
+++ attack-script/attack.js
@@ -0,0 +1,25 @@
+// attack-script/attack.js
+const axios = require('axios');
+
+const PORT = 3005;
+
+const URL = `http://localhost:${PORT}/buy`;
+const REQUESTS = 200; // We have 100 items, but we send 200 requests
+
+async function attack() {
+    console.log(`Starting attack with ${REQUESTS} concurrent requests...`);
+    
+    // Create an array of 200 promises (requests)
+    const requests = [];
+    for (let i = 0; i < REQUESTS; i++) {
+        requests.push(axios.post(URL).catch(err => {
+            return err.response; // Don't crash on error
+        }));
+    }
+
+    // Fire them all at the exact same time
+    await Promise.all(requests);
+    console.log('Attack finished.');
+}
+
+attack();
